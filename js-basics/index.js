7e374852b5a2a339f0d4a844fba4edd3c8da4ad1
Adam Ponting <adam.ponting@gmail.com>
2019-11-14 19:45:55 +0000

    js for beginners done

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,1 +1,25 @@
-console.log('Hello World!')
+
+let selectedColors = ['red', 'blue']
+selectedColors.forEach(function(e, i) {
+    console.log('eeeeee' + e + i)
+})
+console.log(selectedColors.length    )
+selectedColors.forEach((e,i) => {
+    console.log(1+e+i)
+    })
+
+var fruits = ["apple", "orange", "cherry"];
+fruits.forEach(myFunction);
+
+function myFunction(item, index) {
+        console.log('dsdasd', item + index);
+    }
+//   document.getElementById("demo").innerHTML += index + ":" + item + "<br>";
+
+
+function greet(name, lastName) { // name is parameter of greet function
+    console.log('hello world ' + name + ' ' + lastName)
+}
+
+greet('john', 'dasdsad') // john is argument of greet function
+
