31dc77e6cc93b9a4d7696075a47a36b98ab1830e
Adam Ponting <adam.ponting@gmail.com>
2019-12-03 11:15:56 +0000

    updated lint

--- wesbos/beginner_js/playground/node-examples.js
+++ wesbos/beginner_js/playground/node-examples.js
@@ -1,11 +1,7 @@
+/* eslint-disable no-console */
 console.log('im from node');
 function sayhi(name) {
         console.log(name);
 }
 
 sayhi();
-function name(params) {
-        const age = 10;
-        alert(age);
-        alert('dddddd');
-}
